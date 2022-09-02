84f24ab26569560973581669f539181689e68e6f
dduarteingcom <daduarte@estudiantec.cr>
2022-09-01 20:06:05 -0600

    ÚLTIMO PRUEBA
    Solo quiero intentar hacer un push

--- src/main/java/com/example/trabajoextraclase/Main.java
+++ src/main/java/com/example/trabajoextraclase/Main.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
@@ -16,7 +15,6 @@ public class Main extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
